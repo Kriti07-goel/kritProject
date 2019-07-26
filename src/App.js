305b26aaeae749caeb406fa89aa10d6d0e94407c
Kriti07-goel <52536237+Kriti07-goel@users.noreply.github.com>
2019-07-26 18:47:13 +0530

    Add files via upload
    files added in src folder

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import HomePage from './components/Homepage';
+import Cart from './components/Cart';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+} from 'react-router-dom';
 
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Router>
+      <Switch>
+        <Route path="/cart" component={Cart} />
+        <Route path="/" component={HomePage} />
+      </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
